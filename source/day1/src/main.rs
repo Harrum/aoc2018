5510b5dbc9630a241bea0d1e430e47e596e5162b
Harm <harmen_hoogterp@hotmail.com>
2018-12-08 16:59:37 +0100

    day 1

--- source/day1/src/main.rs
+++ source/day1/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::str::FromStr;
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("input.txt")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+
+    let mut start = 0;
+
+    for line in buf_reader.lines() {
+        let l = line.unwrap();
+        
+        let value = i32::from_str(l.get(1..).unwrap()).unwrap();
+
+        if l.starts_with("+") {
+            start += value;
+        }
+        else {
+            start -= value;
+        }
+    }
+
+    println!("{}", start);
+
+    Ok(())
 }
