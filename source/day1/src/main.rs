4ae0488a7806a242be0ddd9af6a427a2d3e82ba7
Harm <harmen_hoogterp@hotmail.com>
2018-12-09 19:23:30 +0100

    part 2

--- source/day1/src/main.rs
+++ source/day1/src/main.rs
@@ -4,9 +4,16 @@ use std::io::prelude::*;
 use std::str::FromStr;
 
 fn main() -> std::io::Result<()> {
+    println!("part 1: {}", part1().unwrap()); 
+
+    println!("part 2: {}", part2().unwrap()); 
+
+    Ok(())
+}
+
+fn part1() -> std::io::Result<i32> {
     let file = File::open("input.txt")?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
+    let buf_reader = BufReader::new(file);
 
     let mut start = 0;
 
@@ -23,7 +30,48 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-    println!("{}", start);
+    Ok(start)
+}
 
-    Ok(())
+fn part2() -> std::io::Result<i32> {
+    let file = File::open("input.txt")?;
+    let buf_reader = BufReader::new(file);
+
+    let mut start = 0;
+    let mut freqs = Vec::new();
+    let mut lines = Vec::new();
+
+    for line in buf_reader.lines() {
+        lines.push(line.unwrap());
+    }
+    
+
+    let mut cont = true;
+    let mut index = 0;
+
+    while cont {
+        freqs.push(start);
+
+        if index >= lines.len() {
+            index = 0
+        }
+
+        let line = lines.get(index).unwrap();
+        let value = i32::from_str(line.get(1..).unwrap()).unwrap();
+
+        if line.starts_with("+") {
+            start += value;
+        }
+        else {
+            start -= value;
+        }
+        index += 1;
+        
+
+        if freqs.contains(&start) {
+            cont = false;
+        }
+    }
+    
+    Ok(start)
 }
